2c37d2f6f60f778eb7f787513ab27250e02c4ebb
atjhoendz <achunarmando@gmail.com>
2018-10-07 00:58:13 +0700

    View login page

---
+++ application/views/loginpage.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>E-Home</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/bootstrap.css" />
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/style.css" />
+    <script src="<?php echo base_url();?>assets/js/bootstrap.js"></script>
+</head>
+<body>
+    <div class="bg blur"></div>
+    <div class="bg-text">
+        <h1><strong>E-Home</strong></h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus molestias mollitia eius dolorum maxime dignissimos quos hic impedit assumenda ab, alias porro corporis et consequuntur quam, temporibus laborum molestiae repudiandae!</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas omnis inventore quidem voluptas repudiandae esse dolorem iusto numquam, aliquid ad eligendi dignissimos at, quae quod velit voluptate tenetur! Laborum, optio.</p>
+    </div>
+    <div class="bg-form">
+        <form>
+            <input type="text" placeholder="Home Name"><br>
+            <input type="password" placeholder="Password"><br>
+            <input type="password" placeholder="Confirm Password"><br>
+            <input class="btn" type="submit" value="Login">
+        </form>
+    </div>
+</body>
+</html>
