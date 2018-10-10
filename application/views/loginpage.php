7b801a7bc783683313094aae439a393fe0008419
atjhoendz <achunarmando@gmail.com>
2018-10-10 12:05:41 +0700

    restyling, make responsive, and a date

--- application/views/loginpage.php
+++ application/views/loginpage.php
@@ -7,24 +7,40 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/bootstrap.css');?>">
     <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/style.css');?>">
-    <script src="<?php echo base_url();?>assets/js/bootstrap.js"></script>
+    <script src="<?php echo base_url('assets/js/bootstrap.js');?>"></script>
 </head>
 <body>
-    <div class="bg blur"></div>
-    <div class="bg-text">
-        <h1><strong>E-Home</strong></h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus molestias mollitia eius dolorum maxime dignissimos quos hic impedit assumenda ab, alias porro corporis et consequuntur quam, temporibus laborum molestiae repudiandae!</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas omnis inventore quidem voluptas repudiandae esse dolorem iusto numquam, aliquid ad eligendi dignissimos at, quae quod velit voluptate tenetur! Laborum, optio.</p>
+    <div class="container-fluid date-container">
+        <div class="row">
+            <div class="col-sm-12 date">
+                <span id="tanggal"></span>
+            </div>
+        </div>
     </div>
-    <div class="bg-form">
-        <form>
-            <input type="text" placeholder="Home Name"><br>
-            <input type="password" placeholder="Password"><br>
-            <input type="password" placeholder="Confirm Password"><br>
-            <input class="btn" type="submit" value="Login"><br>
-            <a class="register" href="<?php echo site_url('register'); ?>"><u>Dont have an account? Register now!</u></a>
-            
-        </form>
+    <div class="container login-container">
+        <div class="row">
+            <div class="col-sm-8 bg-text">
+                <h1><strong>E-Home</strong></h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus molestias mollitia eius dolorum maxime dignissimos quos hic impedit assumenda ab, alias porro corporis et consequuntur quam, temporibus laborum molestiae repudiandae!</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas omnis inventore quidem voluptas repudiandae esse dolorem iusto numquam, aliquid ad eligendi dignissimos at, quae quod velit voluptate tenetur! Laborum, optio.</p>
+            </div>
+            <div class="col-sm-4 form-login-container">
+                <form>
+                    <input type="text" placeholder="Home Name"><br>
+                    <input type="password" placeholder="Password"><br>
+                    <input type="password" placeholder="Confirm Password"><br>
+                    <input class="btn" type="submit" value="Login"><br>
+                    <a href="<?php echo site_url('register'); ?>"><u>Dont have an account? Register now!</u></a>
+                </form>
+            </div>
+        </div>
     </div>
 </body>
+<script type="text/javascript">
+    var timer = setInterval(tanggal, 1000);
+    function tanggal(){
+        var d = new Date();
+        document.getElementById("tanggal").innerHTML = d;
+    }
+</script>
 </html>
