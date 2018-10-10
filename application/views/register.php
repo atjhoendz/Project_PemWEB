c35220426439eeedcd5ab7d5a083141420558a1d
atjhoendz <achunarmando@gmail.com>
2018-10-10 12:22:29 +0700

    restyling and make responsive

--- application/views/register.php
+++ application/views/register.php
@@ -7,22 +7,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/bootstrap.css" />
     <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/style.css" />
-    <script src="<?php echo base_url();?>assets/js/bootstrap.js"></script>
+    <script src="<?php echo base_url('assets/js/bootstrap.js');?>"></script>
 </head>
 <body>
-    <div class="bg">
-        <div class="register-container">
-            <form>
-                <h1 class="register-text">Create Account</h1>
-                <input class="input-register" type="text" placeholder="Username"><br>
-                <input class="input-register" type="password" placeholder="Password"><br>
-                <input class="input-register" type="password" placeholder="Confirm Password"><br>
-                <input class="input-register" type="text" placeholder="Nomor Telepon"><br>
-                <input class="input-register" type="email" placeholder="Email"><br>
-                <input class="register-btn" type="submit" value="Register"><br>
-            </form>
+    <div class="container regist-container">
+        <div class="row">
+            <div class="col-sm-12">
+                <form>
+                    <h1 class="register-text">Create Account</h1>
+                    <input class="input-register" type="text" placeholder="Username"><br>
+                    <input class="input-register" type="password" placeholder="Password"><br>
+                    <input class="input-register" type="password" placeholder="Confirm Password"><br>
+                    <input class="input-register" type="text" placeholder="Nomor Telepon"><br>                
+                    <input class="input-register" type="email" placeholder="Email"><br>
+                    <input class="btn" type="submit" value="Register"><br>
+                </form>
+            </div>
         </div>
     </div>
-    
 </body>
 </html>
