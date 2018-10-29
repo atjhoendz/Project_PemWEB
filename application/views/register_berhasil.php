a30646800e1730c1e5cab6957361af67771b2b27
atjhoendz <achunarmando@gmail>
2018-10-29 10:15:27 +0700

    add icon

--- application/views/register_berhasil.php
+++ application/views/register_berhasil.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Success</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="<?php echo base_url("assets/image/ehome-solid.png");?>">
     <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/bootstrap.css" />
     <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/style.css" />
     <script src="<?php echo base_url('assets/js/bootstrap.js');?>"></script>
