77248b9baec97e473bc8378717d265ade2135416
atjhoendz <achunarmando@gmail>
2018-10-29 10:14:13 +0700

    add back to loginpage link

--- application/controllers/Register.php
+++ application/controllers/Register.php
@@ -1,9 +1,6 @@
 <?php
 class Register extends CI_Controller{
     public function index(){
-
-        $this->load->view('register');
-
         $this->form_validation->set_rules('username', 'Username', 'required');
         $this->form_validation->set_rules('password', 'Password', 'required');
         $this->form_validation->set_rules('confirm_password', 'Password Confirmation', 'required|matches[password]');
@@ -11,11 +8,11 @@ class Register extends CI_Controller{
         $this->form_validation->set_rules('email', 'Email', 'required');
 
         if($this->form_validation->run() === FALSE){
-            echo "Gagal";
+            $this->load->view('register');
         } else {
             $this->load->model('Register_model');
             $this->Register_model->register_akun();
-            $this->load->view('register_berhasil');
+            $this->load->view('Homepage');
         }
     }
 }
