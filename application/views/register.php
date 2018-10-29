6e02f94409f8b9ddfdd373c033b323e3c2da8609
atjhoendz <achunarmando@gmail>
2018-10-29 10:16:04 +0700

    add icon and add back to login page link

--- application/views/register.php
+++ application/views/register.php
@@ -5,12 +5,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Register</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="<?php echo base_url("assets/image/ehome-solid.png");?>">
     <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/bootstrap.css" />
     <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/style.css" />
     <script src="<?php echo base_url('assets/js/bootstrap.js');?>"></script>
 </head>
 <body>
-    <?php echo validation_errors(); ?>
     <div class="container regist-container">
         <div class="row">
             <div class="col-sm-12">
@@ -23,6 +23,7 @@
                     <input class="input-register" type="email" placeholder="Email" name="email"><br>
                     <input class="btn" type="submit" value="Register"><br>
                 </form>
+                <a href="<?php echo site_url();?>"><u>Back to Login Page</u></a>
             </div>
         </div>
     </div>
