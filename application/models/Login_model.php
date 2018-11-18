886df0ec3866fe3c06c012ffa29843e094cc444b
atjhoendz <achunarmando@gmail>
2018-11-19 00:30:54 +0700

    recode login auth

--- application/models/Login_model.php
+++ application/models/Login_model.php
@@ -1,7 +1,7 @@
 <?php
     class Login_model extends CI_Model{
-        public function cek_login($table, $where){
-            return $this->db->get_where($table, $where);
+        public function cek_login($table, $uname, $pwd){
+            return $this->db->select('*')->from($table)->where('username', $uname)->where('password', $pwd)->get();
         }
     }
 ?>
