0611a5411d6f20632e87ad2d1ca51f976f6e0443
atjhoendz <achunarmando@gmail>
2018-11-20 00:46:07 +0700

    add login session handling

--- application/controllers/Homepage.php
+++ application/controllers/Homepage.php
@@ -1,7 +1,27 @@
 <?php
     class Homepage extends CI_Controller{
+
+        function __construct(){
+            parent ::__construct();
+            $this->load->model('home_model');
+        }
+        
         public function index(){
-            $this->load->view('homepage');
+            if($this->session->logged_in){
+                $data['housemate'] = $this->home_model->getHousemate_Model();
+                $this->load->view('homepage', $data);
+            }else{
+                redirect(site_url());
+            }
+        }
+
+        function getHousemate(){
+            echo json_encode($this->home_model->getHousemate_Model());
+        }
+
+        function logout(){
+            $this->session->sess_destroy();
+            redirect(site_url());
         }
     }
 ?>
