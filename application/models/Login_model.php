9ef0b67277f4816c9368ddad2e31273c8c1df59d
atjhoendz <achunarmando@gmail>
2018-11-20 00:45:17 +0700

    add getRUmahID function

--- application/models/Login_model.php
+++ application/models/Login_model.php
@@ -3,5 +3,10 @@
         public function cek_login($table, $uname, $pwd){
             return $this->db->select('*')->from($table)->where('username', $uname)->where('password', $pwd)->get();
         }
+
+        public function getRumahID($uname){
+            $this->db->select('id_rumah')->from('user')->where('username', $uname);
+            return $this->db->get()->result();
+        }
     }
 ?>
