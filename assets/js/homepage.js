8c0baed2045b51c6cf91ed101a175836e005b5c2
atjhoendz <achunarmando@gmail>
2018-11-20 00:49:02 +0700

    add modal popup on housemate

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,4 +1,8 @@
 $(document).ready(function () {
+    var baseUrl = $('#baseUrl').val();
+    var tinggifooter = $('#footer').height();
+    var setTinggi = tinggifooter+'px';
+    $('#main .container').css({"margin-bottom":setTinggi});
     
     //on window resize will set margin-bottom of mainContainer
     $(window).on('resize', function(){
@@ -23,7 +27,6 @@ $(document).ready(function () {
 
     // Home button
     $('#btnHome').on('click', function () {
-        var baseUrl = $('#baseUrl').val();
         window.location.replace(baseUrl);
     });
 
@@ -41,6 +44,66 @@ $(document).ready(function () {
         $('#taskContainer').addClass('hide');
         $('#housemateContainer').removeClass('hide');
         $('#financeContainer').addClass('hide');
+
+        // Get Housemate Data
+        // $.ajax({
+        //     type: "ajax",
+        //     url: baseUrl+"/getHousemate",
+        //     dataType: "json",
+        //     success: function (data) {
+        //         var len = data.length;
+        //         for(var i=0;i<len;i++){
+        //             $('#HousemateSubmenu').append(""+
+        //             "<li>"+
+        //                 "<a href='" + baseUrl + "anggota/"+ i + "'>"+
+        //                     "<i class='fas fa-user'></i>" +
+        //                     "<span>" + data[i].nama_anggota + "</span>" +
+        //                 "</a>" +
+        //             "</li>")
+        //         }
+        //     }
+        // });
+    });
+
+    // Add housemate
+    $('#addHousemate').on('click', function(){
+        $('#modalPopUp').removeClass('hide');
+        $('#modalPopUp').addClass('modalAdd-container');
+        $('#modal-title').text('Add Housemate');
+        $('#modalContent').html(
+            '<form method="POST" class="formAdd">' +
+                '<input class="form-control" type="text" name="nama_anggota" id="nama-anggota" placeholder="Nama Anggota">'+
+                '<input class="form-control" type="text" name="url_foto" id="url-foto" placeholder="URL Foto">'+
+                '<input type="submit" class="btn" value="Add">'+
+            '</form>'
+        );
+    });
+
+    // Edit Housemate
+    $('#editHousemate').on('click', function(){
+        $('#modalPopUp').removeClass('hide');
+        $('#modalPopUp').addClass('modalAdd-container');
+        $('#modal-title').text('Edit Housemate');
+        $('#modalContent').html(
+            '<form method="POST" class="formAdd">' +
+                '<div class="form-group">' +
+                    '<select id="list_housemate" class="form-control select">' +
+                        '<option value="1">nama-anggota</option>' +
+                    '</select>' +
+                '</div>' +
+                '<div class="form-group">' +
+                    '<input class="form-control" type="text" id="nama_anggota" placeholder="Nama Anggota">' +
+                    '<input class="form-control" type="text" id="url_foto" placeholder="URL Foto">' +
+                '</div>' +
+                '<input type="submit" class="btn" value="Update">' +
+            '</form>'
+        );
+    });
+
+    // Close Modal
+    $('#closePopUp').on('click', function(){
+        $('#modalPopUp').addClass('hide');
+        $('#modalPopUp').removeClass('modalAdd-container');
     });
 
     // Sidebar Finance Button
@@ -57,5 +120,5 @@ $(document).ready(function () {
         $('#mainContainer').addClass('hide');
         $('#housemateContainer').addClass('hide');
         $('#financeContainer').addClass('hide');
-    });
+    });   
 });
