88a7df4b5fb0dd003fb7626e8dcf9d4c8bc9f5f1
atjhoendz <achunarmando@gmail>
2018-11-26 23:21:23 +0700

    add new popper js

--- application/views/homepage.php
+++ application/views/homepage.php
@@ -12,7 +12,7 @@
     <!-- Jquery -->
     <script src="<?php echo base_url('assets/js/jquery-3.3.1.js');?>"></script>
     <!-- Popper JS -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>   
+    <script src="<?php echo base_url('assets/js/popper.js'); ?>"></script>   
     <!-- Bootstrap JS -->
     <script src="<?php echo base_url('assets/js/bootstrap.js');?>"></script>
     <!-- Homepage JS -->
