e8a857c640426a33be4c80da0c58a4570596fc87
atjhoendz <achunarmando@gmail>
2018-11-26 23:21:52 +0700

    swap jquery & bootstrap js -> fixing error

--- application/views/loginpage.php
+++ application/views/loginpage.php
@@ -8,8 +8,8 @@
     <link rel="icon" href="<?php echo base_url("assets/image/ehome-solid.png");?>">
     <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/bootstrap.css');?>">
     <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/style.css');?>">
-    <script src="<?php echo base_url('assets/js/bootstrap.js');?>"></script>
     <script src="<?php echo base_url('assets/js/jquery-3.3.1.js');?>"></script>
+    <script src="<?php echo base_url('assets/js/bootstrap.js');?>"></script>
     <script src="<?php echo base_url('assets/js/loginpage.js');?>"></script>
 </head>
 <body>
