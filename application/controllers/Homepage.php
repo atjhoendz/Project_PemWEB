cac66a60eeffc6c6afcc94a8c94f80e7aa68ac72
Fish-IRL <huvuhuvuhuvu@gmail.com>
2018-11-30 10:06:22 +0700

    added line to pass data retrieved from finance model to data array

--- application/controllers/Homepage.php
+++ application/controllers/Homepage.php
@@ -4,11 +4,13 @@
         function __construct(){
             parent ::__construct();
             $this->load->model('home_model');
+            $this->load->model('Finance_model');
         }
         
         public function index(){
             if($this->session->logged_in){
                 $data['housemate'] = $this->home_model->getHousemate_Model();
+                $data['finance'] = $this->Finance_model->getFinance_Model();
                 $this->load->view('homepage', $data);
             }else{
                 redirect(site_url());
