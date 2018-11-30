93c789c6b93fa531c0fdf97d14246be0e70a7b55
Fish-IRL <huvuhuvuhuvu@gmail.com>
2018-11-30 15:00:35 +0700

    added function to retrieve all finance data

--- application/models/Finance_model.php
+++ application/models/Finance_model.php
@@ -6,5 +6,29 @@
             $query = $this->db->get('keuangan');
             return $query->result();
         }
+
+        public function getIncome(){
+            $idRumah = $this->session->idRumah;
+            $this->db->select('*')->where('flag', 1)->where('id_rumah', $idRumah);
+            $query = $this->db->get('keuangan');
+            return $query->result();
+        }
+
+        public function getExpenses(){
+            $idRumah = $this->session->idRumah;
+            $this->db->select('*')->where('flag', 0)->where('id_rumah', $idRumah);
+            $query = $this->db->get('keuangan');
+            return $query->result();
+        }
+
+        public function getBalance(){
+            $idRumah = $this->session->idRumah;
+            $query = $this->db->query("SELECT (
+                (SELECT SUM(jumlah) FROM keuangan WHERE flag=1 && id_rumah='".$idRumah."')
+                - (SELECT SUM(jumlah) FROM keuangan WHERE flag=0 && id_rumah='".$idRumah."')) AS balance
+            FROM keuangan;");
+            
+            return $query->result();
+        }
     }
 ?>
