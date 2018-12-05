b1c8faed3e108f42cd21f8b97aa97786a7bcf4d4
atjhoendz <achunarmando@gmail>
2018-12-06 02:13:26 +0700

    controlling task function

--- application/controllers/Homepage.php
+++ application/controllers/Homepage.php
@@ -5,6 +5,7 @@
             parent ::__construct();
             $this->load->model('home_model');
             $this->load->model('Finance_model');
+            $this->load->model('task_model');
         }
         
         public function index(){
@@ -14,6 +15,7 @@
                 $data['expenses'] = $this->Finance_model->getExpenses();
                 $data['income'] = $this->Finance_model->getIncome();
                 $data['balance'] = $this->Finance_model->getBalance();
+                $data['task'] = $this->task_model->getTask_model();
                 $this->load->view('homepage', $data);
             }else{
                 redirect(site_url());
@@ -98,6 +100,71 @@
                 echo 'Success';
             }else{
                 echo 'Gagal Menambahkan Data';
+            }
+        }
+
+        function getTask(){
+            echo json_encode($this->task_model->getTask_model());
+        }
+
+        function addTask(){
+            $id_task = substr($this->input->post('taskDetail'), 0, 3) . random_int(0, 99);
+            $id_rumah = $this->session->idRumah;
+            $id_anggota = $this->input->post('idAnggota');
+            $taskDetail = $this->input->post('taskDetail');
+            $nama_anggota = $this->input->post('namaAnggota');
+            $deadline = $this->input->post('deadlineTask');
+
+            $data_task = array(
+                'id_task' => $id_task,
+                'id_anggota' => $id_anggota,
+                'id_rumah' => $id_rumah,
+                'nama_task' => $taskDetail,
+                'nama_anggota' => $nama_anggota,
+                'tgl_task' => $deadline
+            );
+
+            if($this->task_model->addTask_model($data_task)){
+                echo 'Success';
+            }else{
+                echo 'Gagal Menambahkan Data';
+            }
+        }
+
+        function updateTask(){
+            $id_task = $this->input->post('idTask');
+            $id_rumah = $this->session->idRumah;
+            $id_anggota = $this->input->post('idAnggota');
+            $taskDetail = $this->input->post('taskDetail');
+            $nama_anggota = $this->input->post('namaAnggota');
+            $deadline = $this->input->post('deadlineTask');
+
+            $data_task = array(
+                'id_task' => $id_task,
+                'id_anggota' => $id_anggota,
+                'id_rumah' => $id_rumah,
+                'nama_task' => $taskDetail,
+                'nama_anggota' => $nama_anggota,
+                'tgl_task' => $deadline
+            );
+
+            if($this->task_model->updateTask_model($data_task)){
+                echo 'Success';
+            }else{
+                echo 'Gagal Memperbarui Data';
+            }
+        }
+
+        function deleteTask(){
+            $id_task = $this->input->post('idTask');
+            $data_task = array(
+                'id_task' => $id_task
+            );
+
+            if($this->task_model->deleteTask_model($data_task)){
+                echo 'Success';
+            }else{
+                echo 'Gagal Menghapus Data';
             }
         }
 
